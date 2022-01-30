8a5d027fbc8bf5ad0148f8af202467d6e8117b30
Ahmed Youssef <a_yousseff@icloud.com>
2022-01-30 20:41:24 +0100

    Remove messages

--- src/main/java/com/mk/cli/command/EchoCommand.java
+++ src/main/java/com/mk/cli/command/EchoCommand.java
@@ -16,11 +16,6 @@ public class EchoCommand {
 	@ShellMethod("Displays greeting message to the user whose name is supplied")
 	public String echo(@ShellOption({ "-N", "--name" }) String name) {
 		 String message = String.format("Hello %s!", name);
-	        shellHelper.print(String.format("Welcome %s!", name ));
-	        shellHelper.printError(String.format("Welcome %s!", name ));
-	        shellHelper.printInfo(String.format("Welcome %s!", name ));
-	        shellHelper.printSuccess(String.format("Welcome %s!", name ));
-	        shellHelper.printWarning(String.format("Welcome %s!", name ));
 	        String output = shellHelper.getSuccessMessage(message);
 	        return output.concat(" You are running spring shell cli-demo.");
 	}
